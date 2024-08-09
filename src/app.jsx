0834f0f1af35256dbe079cbc60d13ac3257464ba
Yuri Alves <yuripiresalves@gmail.com>
2024-08-09 17:18:32 -0300

    add lib shadcnui

---
+++ src/app.jsx
@@ -0,0 +1,12 @@
+import './index.css'
+
+import { Input } from './components/ui/input'
+
+export function App() {
+  return (
+    <div className="container">
+      <h1>Hello World</h1>
+      <Input type="text" placeholder="Type something..." />
+    </div>
+  )
+}
